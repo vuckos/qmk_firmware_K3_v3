af956497851573c95192f69935fba592fa14e1eb
lokher <lokher@gmail.com>
2025-03-22 14:59:17 +0800

    Added K/Q Pro series keyboards

--- keyboards/keychron/k8_max/mcuconf.h
+++ keyboards/keychron/k8_max/mcuconf.h
@@ -1,4 +1,4 @@
-/* Copyright 2024 @ lokher (https://www.keychron.com)
+/* Copyright 2024 ~ 2025 @ lokher (https://www.keychron.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
